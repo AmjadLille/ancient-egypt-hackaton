8c8bf1cb5e502ad259a51b8d278e4d6bd35bf8a9
AmjadWebDev <amjadwebdev@gmail.com>
2020-11-19 21:06:47 +0100

    fin css

--- egypt/src/components/traduire-page/test.js
+++
@@ -1,6 +0,0 @@
-const test = (i) => {
-  let str = i.split("");
-  console.log(str);
-};
-
-test("amjad");
--- egypt/src/components/traduire-page/traduire.jsx
+++ egypt/src/components/traduire-page/traduire.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Title, Container, Label, Text, Result, Trad, Header } from "./traduire.style";
 
 class Traduire extends React.Component {
   constructor() {
@@ -15,7 +16,11 @@ class Traduire extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const word = this.state.text.toUpperCase().replace(/\//g, "").split("").filter(Boolean);
+    const word = this.state.text
+      .toUpperCase()
+      .replace(/[^a-zA-Z]+/g, "")
+      .split("")
+      .filter(Boolean);
     this.setState({ trad: word });
     console.log(this.state.trad);
     this.setState({ text: "" });
@@ -24,16 +29,27 @@ class Traduire extends React.Component {
   render() {
     const { text, trad } = this.state;
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Traduire ENG to hieroglyphics
-            <textarea value={text} onChange={this.handleChange} rows={5} cols={50} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        {trad.map((num) => (typeof num == "string" ? <img src={`/images/${num}.jpg`} alt="icon" /> : null))}
-      </div>
+      <Container>
+        <Header>
+          <img src="/images/eye.gif" alt="eye" />
+          <Title style={{ fontFamily: "'Big Shoulders Inline Text', cursive" }}>See your Words as written in Egyptian Hieroglyphics </Title>
+          <img src="/images/eye.gif" alt="eye" />
+        </Header>
+        <Label style={{ fontFamily: "'Big Shoulders Inline Text', cursive" }}>Transfer English to hieroglyphics</Label>
+        <Trad>
+          <form onSubmit={this.handleSubmit}>
+            <Text value={text} onChange={this.handleChange} rows={5} cols={50} />
+            <input type="submit" value="Translate" style={{ marginTop: "15px", height: "3vh", fontSize: "18px" }} />
+          </form>
+        </Trad>
+
+        <Result>
+          {trad.map((num) => (
+            <img src={`/images/${num}.jpg`} alt="icon" />
+          ))}
+        </Result>
+        <img src="/images/egyptloop.gif" alt="" style={{ marginTop: "8vh" }} />
+      </Container>
     );
   }
 }
