09c02da22c8f4084d61847095a350b473b4d34cd
frvanco <antoinevancompernolle@gmail.com>
2020-11-20 00:23:48 +0100

    some more text

--- egypt/src/components/Dieu.jsx
+++ egypt/src/components/Dieu.jsx
@@ -16,20 +16,88 @@ const Title = styled.h1`
 `;
 
 const StyledComponents = () => (
-  <Container>
-    <Title>
-      Osiris était un roi sage qui étaitmarié à sa sœur, Isis. Lorsqu'il fut
-      assassiné par leur frère Seth, elleusa de magie pour le ressusciter dans
-      le but d’avoir de lui un fils.Celui s’appela Horus et il vint venger la
-      mort de son père enassassinant son oncle. Horus est ensuite devenu le roi
-      d'Égypte, tandisqu'Osiris est devenu le roi des morts et le juge des
-      enfers. LesÉgyptiens le considéraient comme triomphant de la mort ; chaque
-      pharaondeviendrait Osiris après sa mort alors que, de son vivant, il
-      étaitl'incarnation d'Horus. Osiris était également le dieu de la
-      végétationet était donc souvent peint la peau verte – le vert symbolisant
-      alors lacroissance nouvelle et la renaissance.
-    </Title>
-  </Container>
+  <div>
+    <Container>
+      <Title>
+        Osiris était un roi sage qui étaitmarié à sa sœur, Isis. Lorsqu'il fut
+        assassiné par leur frère Seth, elleusa de magie pour le ressusciter dans
+        le but d’avoir de lui un fils.Celui s’appela Horus et il vint venger la
+        mort de son père enassassinant son oncle. Horus est ensuite devenu le
+        roi d'Égypte, tandisqu'Osiris est devenu le roi des morts et le juge des
+        enfers. LesÉgyptiens le considéraient comme triomphant de la mort ;
+        chaque pharaondeviendrait Osiris après sa mort alors que, de son vivant,
+        il étaitl'incarnation d'Horus. Osiris était également le dieu de la
+        végétationet était donc souvent peint la peau verte – le vert
+        symbolisant alors lacroissance nouvelle et la renaissance.
+      </Title>
+    </Container>
+    <Container>
+      <Title>
+        Isis était l'ultime déesse car elle était la mère d'Horus et à la fois
+        épouse et sœur d'Osiris. Lorsque son mari bien-aimé fut assassiné par
+        leur frère Seth, elle rassembla les parties démembrées du corps d'Osiris
+        et les réunit avec des bandages, jetant ainsi les bases de la pratique
+        de l'Égypte ancienne qui consistait à momifier les défunts. En ramenant
+        Osiris à la vie, Isis a également introduit le concept de résurrection
+        qui a profondément influencé d'autres religions également, y compris le
+        christianisme. Isis, représentée comme une femme tenant un Ankh dans sa
+        main et parfois avec un corps de femme et une tête de vache ou des
+        cornes de vache, était également vénérée comme déesse de la fertilité.
+      </Title>
+    </Container>
+    <Container>
+      <Title>
+        L'un des plus importants Dieux Égyptiens était le fils d'Osiris et
+        d'Isis. Selon le célèbre mythe, il a vengé le meurtre de son père en
+        assassinant son oncle Seth, ce pourquoi il devint le nouveau roi
+        d'Égypte. Les premiers pharaons égyptiens ont par la suite légitimé
+        leurs règnes en se présentant comme l’incarnation d’Horus. Également
+        vénéré comme le dieu de la lumière et du ciel, Horus était généralement
+        représenté comme une créature mâle à tête de faucon arborant une
+        couronne blanche et rouge.
+      </Title>
+    </Container>
+    <Container>
+      <Title>
+        Le dieu de la sagesse, de l'écriture et de la magie était le plus
+        souvent représenté sous la forme d’un homme à tête d'ibis. Thot était le
+        scribe du monde souterrain, qui enregistrait un verdict concernant
+        chaque défunt dans la salle de la Maât, entretenait la bibliothèque des
+        dieux, écrivait les sorts du "Livre des morts" et rédigeait "Le Livre de
+        Thot" (contenant les secrets de l'univers). Mais il a également joué un
+        rôle important dans de nombreux mythes de l'Égypte ancienne, agissant
+        comme arbitre entre les forces du bien et du mal.
+      </Title>
+    </Container>
+    <Container>
+      <Title>
+        Râ (ou Rê) était le dieu du soleil et l'une des divinités plus
+        importantes de l’Égypte antique. Il fut associé à la construction des
+        pyramides et à la résurrection des pharaons. Symboliquement, il naissait
+        chaque matin au lever du soleil ; il mourrait symboliquement de la même
+        façon à chaque coucher de soleil. Chaque nuit, il commençait un voyage
+        dans le Monde souterrain. Plus tard, Rê fut étroitement associé à Horus
+        et, tout comme ce dernier, il était généralement représenté par un homme
+        à tête de faucon. Cependant, plutôt qu’une couronne blanche et rouge, Râ
+        était représenté portant un disque solaire sur la tête.
+      </Title>
+    </Container>
+    <Container>
+      <Title></Title>
+    </Container>
+    <Container>
+      <Title></Title>
+    </Container>
+    <Container>
+      <Title></Title>
+    </Container>
+    <Container>
+      <Title></Title>
+    </Container>
+    <Container>
+      <Title></Title>
+    </Container>
+  </div>
 );
 
 export default StyledComponents;
