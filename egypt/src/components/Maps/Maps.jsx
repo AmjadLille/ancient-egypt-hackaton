cd6f927b4784f601ef1221876346b94d422f047f
Thoms-C <thomas.clment@gmail.com>
2020-11-20 13:05:07 +0100

    maps finished

--- egypt/src/components/Maps/Maps.jsx
+++ egypt/src/components/Maps/Maps.jsx
@@ -26,7 +26,8 @@ const Height = styled.div`
 
 const Main = styled.div`
   width: 100%;
-  background-color: white;
+  background-image: url("https://wallpaperaccess.com/full/1659875.jpg");
+  
 `;
 
 const CarouselStyle = styled.img`
@@ -37,7 +38,7 @@ const Title = styled.h1`
   color: white;
   text-shadow: 1px 1px 2px black;
   text-align: center;
-  font-size: 2em;
+  font-size: 2.5em;
   margin: 0;
   padding: 2vh 0 2vh 0;
   border-top: 2px solid black;
@@ -50,6 +51,47 @@ const Title2 = styled.h2`
   border-bottom: 2px solid black;
   text-align: center;
   padding: 2vh 0 2vh 0;
+  margin: 0;
+  font-size: 2em;
+  color: white;
+  text-shadow: 1px 1px 2px black;
+  background-image: url("https://wallpaperaccess.com/full/1659875.jpg");
+`
+const TitleP = styled.h2`
+  border-top: 3px solid white;
+  border-bottom: 3px solid white;
+  text-align: center;
+  padding: 2vh 0 2vh 0;
+  margin: 0;
+  color: white;
+  text-shadow: 1px 1px 2px black;
+`
+
+const Text = styled.p`
+  padding: 0 5vw 0 5vw;
+  text-align: center;
+  font-size: 1.5em;
+  color: white;
+  text-shadow: 1px 1px 2px black;
+`
+
+const TextLi = styled.p`
+  color: white;
+  text-shadow: 1px 1px 2px black;
+`
+
+const StyleLi = styled.li`
+  font-size: 1.5em;
+  padding: 0 5vw 0 5vw;
+  text-align: center;
+`
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+`
+
+const MapStyle = styled.div`
+  border: 6px solid gold;
 `
 
 class Maps extends Component {
@@ -288,41 +330,68 @@ class Maps extends Component {
     return (
       <Main>
         <Title style={{ fontFamily: "'Oswald', sans-serif" }}>Our most iconic monuments</Title>
-        <Carousel showArrows={true} showThumbs={true}>
+        <Carousel showArrows={true} showThumbs={false}>
           <div>
             <CarouselStyle src="https://c.pxhere.com/photos/d8/54/temple_of_isis_philae_island_egypt_nile_river_temple_ruins_ancient-1041538.jpg!d" />
-            <p className="legend">Le Temple d’Isis à Philae</p>
+            <p className="legend">The Temple of Isis at Philae</p>
           </div>
           <div>
             <CarouselStyle src="https://azimutmonde.files.wordpress.com/2014/03/dsc00712.jpg" />
-            <p className="legend">Fresque du temple du luxor</p>
+            <p className="legend">Fresco of the temple of luxor</p>
           </div>
           <div>
             <CarouselStyle src="https://image.jimcdn.com/app/cms/image/transf/dimension=4096x4096:format=jpg/path/s9ab19a6bf391d9a3/image/ie351ff75d9ab1396/version/1558250526/image.jpg" />
-            <p className="legend">Les Colosses de Memnon</p>
+            <p className="legend">The Colossi of Memnon</p>
           </div>
           <div>
             <CarouselStyle src="https://cdn.futura-sciences.com/buildsv6/images/wide1920/6/a/1/6a19c310d8_118395_temple-abou-simbel-egypte.jpg" />
-            <p className="legend">Le Temple d'Abu Simbel</p>
+            <p className="legend">Abu Simbel Temple</p>
           </div>
           <div>
             <CarouselStyle src="https://i.pinimg.com/originals/ba/a7/47/baa747b106ab9b82e62e27e0d3c6af92.jpg" />
-            <p className="legend">Le Temple de Sobek et Haroëris</p>
+            <p className="legend">The Temple of Sobek and Haroëris</p>
           </div>
         </Carousel>
-        <Title2>You should see</Title2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum distinctio labore id itaque hic, delectus modi illum iure quidem sit adipisci debitis dolorem tenetur necessitatibus recusandae? Necessitatibus officia et laboriosam.
-          Aspernatur ex distinctio excepturi magnam! Delectus possimus commodi mollitia, corporis beatae tenetur, saepe eveniet quam nihil dignissimos maxime eligendi molestiae necessitatibus asperiores voluptatem cumque numquam deserunt itaque provident! Nesciunt, quos!
-          Illo sequi cumque officiis at, facere, illum dolore quis, fuga sed placeat eum alias debitis! Asperiores ducimus saepe, veniam dicta officiis sint itaque ipsum excepturi voluptatum harum natus in error.
-          Vitae corrupti voluptates dicta fugit necessitatibus possimus saepe similique esse quis repellat eius veniam, nam, a dolore qui eos earum iusto recusandae repudiandae rem perferendis incidunt explicabo voluptas aut. Numquam.
-          Tenetur labore reiciendis libero ab alias, beatae praesentium neque ipsum veniam quae porro blanditiis fuga illum velit reprehenderit similique. Maiores, cum voluptatibus. Mollitia saepe a dignissimos accusamus praesentium, numquam nisi.
-          Sed sunt dolores inventore ab repellendus maiores, numquam natus tempora quo dignissimos temporibus quibusdam voluptate. Adipisci repellendus vero vitae laborum, id error maiores aspernatur quam dolore iste aut consequuntur ipsam.
-          Illum porro adipisci, amet totam cum fugiat. Quis, modi quod. Nihil ut temporibus modi corrupti eos voluptatibus corporis? Similique dolor cum saepe est repellat natus, dolorem excepturi fuga? Suscipit, quam?
-          Sed, esse veritatis modi facilis reprehenderit quae eum. Quis debitis earum minus labore, qui ea quam dolorem? Cum, dolorum. Inventore minima voluptatem impedit corrupti beatae, ratione facilis magni omnis ea!
-          Et, dignissimos dicta nostrum aut nemo obcaecati vero praesentium quasi eos veritatis? Pariatur voluptas error nulla sunt excepturi sit suscipit alias libero, deserunt, architecto possimus! Atque quaerat dolor libero aut?
-          Veritatis beatae nostrum excepturi iure praesentium dolorum libero labore eos laudantium ab suscipit inventore, adipisci voluptatem, in sint dicta! Laudantium animi quasi in, atque voluptate officia! Ea laudantium blanditiis repellat!
-        </p>
+        <Title2>What you should see</Title2>
+        <Text>
+          Home of the Pharaohs, Egypt is a dazzling destination of temples and tombs that wow all who visit. It's not all historic treasures, though. With vast tracts of desert, and the famed Nile River, there's something for everyone here.
+          <br></br>Egypt has so much for travelers to see and do, it's the perfect country for a mix of activities combining culture, adventure, and relaxation. Find the best places to visit with our list of the top monuments in Egypt.
+        </Text>
+        <StyledUl>
+          <StyleLi>
+            <TitleP>Temple of Hatshepsut</TitleP>
+            <TextLi> It is a colonnaded structure, which was designed and implemented by Senemut, the royal architect of Hatshepsut, to serve for her posthumous worship and to honor the glory of Amun. The temple is built into a cliff face that rises sharply above it and consists of three layered terraces reaching 30 meters (97 ft ) in height. These terraces are connected by long ramps which were once surrounded by gardens.</TextLi>
+            </StyleLi>
+          <StyleLi>
+          <TitleP>Bent Pyramid</TitleP>
+            <TextLi>The Bent Pyramid located at Dahshur was the second pyramid built by pharaoh Sneferu. Mysteriously, the pyramid rises from the desert at an angle of 55 degrees and then suddenly changes to a more gradual angle of 43 degrees. One theory holds that due to the steepness of the original angle the weight to be added above the inner chambers and passageways became to large, forcing the builders to adopt a shallower angle. It is the only pyramid in Egypt of which the outer casing of polished limestone is still largely intact.</TextLi>
+            </StyleLi>
+          <StyleLi>
+          <TitleP>Step Pyramid of Djoser</TitleP>
+            <TextLi>The Step Pyramid of Djoser at the Saqqara necropolis was the very first pyramid built by the ancient Egyptians. It was constructed during the 27th century BC for the burial of Pharaoh Djoser by his Vizier Imhotep. The ancient monument began as a traditional, flat-roofed mastaba but by the end of Djoser’s reign it had risen to a six stepped layer Pyramid and stood 204 feet (62 meters) high. As in earlier mastaba tombs, the burial chambers of the Step Pyramid are underground, hidden in a maze of tunnels.</TextLi>
+            </StyleLi>
+          <StyleLi>
+          <TitleP>Luxor Temple</TitleP>
+            <TextLi>The Luxor temple was dedicated to the three Egyptian gods Amun, Mut, and Chons. The ancient temple was the center of the festival of Opet, Thebes’ most important festival. During the annual festival the statues of the three Gods were escorted to the temple of Luxor along the avenue of sphinxes that connect the 2 temples.</TextLi>
+            </StyleLi>
+          <StyleLi>
+          <TitleP>Red Pyramid</TitleP>
+            <TextLi>Built by Pharaoh Sneferu, the Red Pyramid is the world’s first successful attempt at constructing a “true” smooth-sided pyramid. At 104 meters it is the 4th highest pyramid ever built in Egypt. What really makes the Red Pyramid special today is the lack of crowds that plagues the Giza Plateau and the comparatively unregulated interior access.</TextLi>
+            </StyleLi>
+          <StyleLi>
+          <TitleP>Abu Simbel</TitleP>
+            <TextLi>The twin temples were originally carved out of the mountainside during the reign of Pharaoh Ramesses The Great in the 13th century BC, as a lasting monument to himself and his queen Nefertari. </TextLi>
+            </StyleLi>
+          <StyleLi>
+          <TitleP>Karnak</TitleP>
+            <TextLi>The Temple of Karnak actually consists of three main temples, smaller enclosed temples, and several outer temples located about 2.5 kilometers north of Luxor. One of most famous structures of Karnak is the Hypostyle Hall, a hall area of 5,000 m2 (50,000 sq ft) with 134 massive columns arranged in 16 rows. <br></br>Most of the work on Karnak was done by the pharaohs of the New Kingdom</TextLi>
+            </StyleLi>
+          <StyleLi>
+          <TitleP>Pyramids of Giza</TitleP>
+            <TextLi>The pyramids in Giza were built over the span of three generations – by Khufu, his second reigning son Khafre, and Menkaure. The Great Pyramid of Khufu is the oldest and sole remnant of the Seven Wonders of the Ancient World. Over 2 million blocks of stone were used to construct the pyramid, during a 20 year period concluding around 2560 BC. The pyramid is an awe-inspiring 139 meters (455 feet) high making it the largest pyramid in Egypt, although nearby Khafre’s Pyramid appears to be larger as it is build at a higher elevation.</TextLi>
+            </StyleLi>
+        </StyledUl>
+        <MapStyle>
         <MapContainer
           className="map"
           center={positionHabouIcon}
@@ -331,7 +400,7 @@ class Maps extends Component {
           <Height>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
             />
             <Marker position={positionHabouIcon} icon={this.HabouIcon}>
               <Popup>Médinet Habou</Popup>
@@ -383,6 +452,7 @@ class Maps extends Component {
             </Marker>
           </Height>
         </MapContainer>
+        </MapStyle>
       </Main>
     );
   }
